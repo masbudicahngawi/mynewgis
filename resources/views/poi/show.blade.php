3e491898765e64794001b3a8de532e4952a0fb9b
ocsigsampit <oc.sig.sampit@gmail.com>
2024-04-25 16:17:11 +0700

    menampilkan deskripsi

--- resources/views/poi/show.blade.php
+++ resources/views/poi/show.blade.php
@@ -1,137 +1,137 @@
 @extends('master')
 
 @section('isi')
-<div id="map"></div>
+    <div id="map"></div>
 @endsection
 
 
 @section('skrip')
-<script type="text/javascript">
+    <script type="text/javascript">
         // center of the map
-    var center = [-7.5563983, 110.8208331];
+        var center = [-7.5563983, 110.8208331];
 
         // Create the map
-    var map = L.map('map').setView(center, 7);
+        var map = L.map('map').setView(center, 7);
 
         // Set up the OSM layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors;'
-    }).addTo(map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors;'
+        }).addTo(map);
 
-    var klaster = L.markerClusterGroup();
+        var klaster = L.markerClusterGroup();
 
         // Fungsi Pencarian
-    var controlSearch = new L.Control.Search({
-        position: 'topright',
-        layer: klaster,
+        var controlSearch = new L.Control.Search({
+            position: 'topright',
+            layer: klaster,
             // propertyName: 'nama',
-        initial: false,
-        zoom: 17,
-        marker: false,
-        collapse: false
-    });
+            initial: false,
+            zoom: 17,
+            marker: false,
+            collapse: false
+        });
 
-    map.addControl(controlSearch);
+        map.addControl(controlSearch);
 
         // Setting Icon
-    var list_icon = [
-        '',
-        '/assets/icons/pinanyar.png',
-        '/assets/icons/tempat_makan.png',
-        '/assets/icons/atm.png',
-        '/assets/icons/spbu.png',
-        '/assets/icons/kedai.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/pin_merah.png',
-        '/assets/icons/hotel.png',
+        var list_icon = [
+            '',
+            '/assets/icons/pinanyar.png',
+            '/assets/icons/tempat_makan.png',
+            '/assets/icons/atm.png',
+            '/assets/icons/spbu.png',
+            '/assets/icons/kedai.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/pin_merah.png',
+            '/assets/icons/hotel.png',
         ];
 
         //---------------------------------------------
 
-    var LeafIcon = L.Icon.extend({
-        options: {
-            iconSize: [32, 37],
-            iconAnchor: [16, 37],
-            popupAnchor: [1, -30]
-        }
-    });
+        var LeafIcon = L.Icon.extend({
+            options: {
+                iconSize: [32, 37],
+                iconAnchor: [16, 37],
+                popupAnchor: [1, -30]
+            }
+        });
 
 
         // Menangkap variable $data dari Controller POI
-    var data_db = {{ Illuminate\Support\Js::from($data) }};
+        var data_db = {{ Illuminate\Support\Js::from($data) }};
 
         // alert("data db : " + data_db);
 
-    var arr = [];
+        var arr = [];
 
-    for (i in data_db) {
+        for (i in data_db) {
 
-        if (data_db[i].objek == "marker") {
-            arr.push({
-                "lokasi": [data_db[i].longitude, data_db[i].latitude],
-                "nama": data_db[i].nama,
-                "jenis": "marker",
-                "kode": data_db[i].jenis_id
-            });
-        } else if (data_db[i].objek == "polygon") {
-            arr.push({
-                "lokasi": JSON.parse(data_db[i].koordinat_polygon),
-                "nama": data_db[i].nama,
-                "jenis": "polygon",
-                "kode": data_db[i].jenis_id
-            });
+            if (data_db[i].objek == "marker") {
+                arr.push({
+                    "lokasi": [data_db[i].longitude, data_db[i].latitude],
+                    "nama": data_db[i].nama,
+                    "deskripsi": data_db[i].deskripsi,
+                    "jenis": "marker",
+                    "kode": data_db[i].jenis_id
+                });
+            } else if (data_db[i].objek == "polygon") {
+                arr.push({
+                    "lokasi": JSON.parse(data_db[i].koordinat_polygon),
+                    "nama": data_db[i].nama,
+                    "deskripsi": data_db[i].deskripsi,
+                    "jenis": "polygon",
+                    "kode": data_db[i].jenis_id
+                });
+            }
         }
-    }
 
 
 
-    for (item in arr) {
-        var nama = arr[item].nama;
-            // alert(arr[item].nama + " -- " + arr[item].jenis + " : " + arr[item].lokasi);
+        for (item in arr) {
+            var nama = arr[item].nama;
+            var deskripsinya = (arr[item].deskripsi == null) ? "No Data" : arr[item].deskripsi;
 
-        if (arr[item].jenis == "marker") {
-            var lokasi = arr[item].lokasi;
-            var kode = arr[item].kode;
-            var obyek = new L.Marker(new L.latLng(lokasi), {
-                icon: new LeafIcon({
-                    iconUrl: list_icon[kode]
-                }),
-                title: nama
-            }).bindPopup(
-            `<div class="card rounded-0" style="width: 15rem;">
+            if (arr[item].jenis == "marker") {
+                var lokasi = arr[item].lokasi;
+                var kode = arr[item].kode;
+                var obyek = new L.Marker(new L.latLng(lokasi), {
+                    icon: new LeafIcon({
+                        iconUrl: list_icon[kode]
+                    }),
+                    title: nama
+                }).bindPopup(
+                    `<div class="card rounded-0" style="width: 15rem;">
             <div class="card-body">
             <h5 class="card-title">${nama}</h5>
-            <p class="card-text">bla.. bla.. bla...</p>
-            <a href="#" class="btn btn-primary rounded-0">Tambah Foto</a>
+            <p class="card-text">${deskripsinya}</p>
             </div>
             </div>`);
 
-            klaster.addLayer(obyek);
+                klaster.addLayer(obyek);
 
-        } else if (arr[item].jenis == "polygon") {
-            var lokasi = arr[item].lokasi;
-            var obyek = new L.Polygon(lokasi, {
-                title: nama
-            }).bindPopup(`<div class="card rounded-0" style="width: 15rem;">
+            } else if (arr[item].jenis == "polygon") {
+                var lokasi = arr[item].lokasi;
+                var obyek = new L.Polygon(lokasi, {
+                    title: nama
+                }).bindPopup(`<div class="card rounded-0" style="width: 15rem;">
             <div class="card-body">
             <h5 class="card-title">${nama}</h5>
-            <p class="card-text">bla.. bla.. bla..</p>
-            <a href="#" class="btn btn-primary rounded-0">Tambah Foto</a>
+            <p class="card-text">${deskripsinya}</p>
             </div>
             </div>`);
 
-            klaster.addLayer(obyek);
+                klaster.addLayer(obyek);
 
+            }
         }
-    }
 
-    map.addLayer(klaster);
-</script>
+        map.addLayer(klaster);
+    </script>
 @endsection
--- resources/views/poi/tambah.blade.php
+++ resources/views/poi/tambah.blade.php
@@ -1,60 +1,59 @@
 @extends('master')
 
 @section('isi')
-<div id="map"></div>
-
+    <div id="map"></div>
 @endsection
 
 
 @section('skrip')
-<script type="text/javascript" defer>
-    // center of the map
-    var center = [-7.5563983, 110.8208331];
+    <script type="text/javascript" defer>
+        // center of the map
+        var center = [-7.5563983, 110.8208331];
 
-    // Create the map
-    var map = L.map('map').setView(center, 7);
+        // Create the map
+        var map = L.map('map').setView(center, 7);
 
-    // Set up the OSM layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors;'
-    }).addTo(map);
+        // Set up the OSM layer
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors;'
+        }).addTo(map);
 
-    var klaster = L.markerClusterGroup();
+        var klaster = L.markerClusterGroup();
 
-    // Menu penambah objek
-    var editableLayers = new L.FeatureGroup();
+        // Menu penambah objek
+        var editableLayers = new L.FeatureGroup();
 
-    map.addLayer(editableLayers);
+        map.addLayer(editableLayers);
 
-    var drawControl = new L.Control.Draw({
-        draw : {
-            polyline : true,
-            circlemarker : false
-        },
-        edit : {
-            featureGroup : editableLayers
-        }
-    });
+        var drawControl = new L.Control.Draw({
+            draw: {
+                polyline: true,
+                circlemarker: false
+            },
+            edit: {
+                featureGroup: editableLayers
+            }
+        });
 
-    map.addControl(drawControl);
+        map.addControl(drawControl);
 
-    // Menangkap variable $jenis dari Controller POI
-    var data_jenis = {{ Illuminate\Support\Js::from($jenis) }};
-    var pilihan = data_jenis.map(item => `<option value="${item.id}">${item.nama}</option>`);
+        // Menangkap variable $jenis dari Controller POI
+        var data_jenis = {{ Illuminate\Support\Js::from($jenis) }};
+        var pilihan = data_jenis.map(item => `<option value="${item.id}">${item.nama}</option>`);
 
-    // Saat objek terbentuk
-    map.on('draw:created', function(e) {
-        var type    = e.layerType;
-        var layer   = e.layer;
+        // Saat objek terbentuk
+        map.on('draw:created', function(e) {
+            var type = e.layerType;
+            var layer = e.layer;
 
-        if (type === 'marker') {
-            var lat = layer.getLatLng().lat;
-            var lng = layer.getLatLng().lng;
+            if (type === 'marker') {
+                var lat = layer.getLatLng().lat;
+                var lng = layer.getLatLng().lng;
 
-            // alert('Titik koordinat : ' + lat + ', ' + lng);
+                // alert('Titik koordinat : ' + lat + ', ' + lng);
 
-            layer.bindPopup(`<form>
+                layer.bindPopup(`<form>
                 @csrf
                 <table>
                 <input type="hidden" id="hid_lat" value="${lat}">
@@ -84,60 +83,62 @@
                 </tr>
                 </table>
                 </form>`);
-        }
+            }
 
-        editableLayers.addLayer(layer);
-    });
+            editableLayers.addLayer(layer);
+        });
 
-    map.addLayer(klaster);
+        map.addLayer(klaster);
 
-    // Simpan
+        // Simpan
 
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
-    $("body").on("click","#tmb_simpan", function (event) {
-        event.preventDefault();
-        let nama        = $("#nama_spot").val();
-        let jenis_id    = $("#jenis_spot").val();
-        let lat         = $("#hid_lat").val();
-        let lng         = $("#hid_lng").val();
-        let deskripsi   = $("#deskripsi").val();
+        $("body").on("click", "#tmb_simpan", function(event) {
+            event.preventDefault();
+            let nama = $("#nama_spot").val();
+            let jenis_id = $("#jenis_spot").val();
+            let lat = $("#hid_lat").val();
+            let lng = $("#hid_lng").val();
+            let deskripsi = $("#deskripsi").val();
 
-        if (confirm("Simpan data ini ?")) {
+            if (confirm("Simpan data ini ?")) {
 
-            $.ajax({
-                url: "{{ route('poi.store') }}",
-                type : "post",
-                data : {
-                    mode : "marker",
-                    nama : nama,
-                    jenis_id : jenis_id,
-                    lat : lat,
-                    lng : lng,
-                    deskripsi : deskripsi
-                },
-                success: function(hasil){
-                    if (hasil.success == true) {
-                        // alert(hasil.message);
-                        toastr.options = { "closeButton" : true,"progressBar" : true }
-                        toastr.success("Data berhasil disimpan.");
-                    }else{
-                        // alert("Error !");
-                        toastr.options =
-                        {
-                            "closeButton" : true,
-                            "progressBar" : true
+                $.ajax({
+                    url: "{{ route('poi.store') }}",
+                    type: "post",
+                    data: {
+                        mode: "marker",
+                        nama: nama,
+                        jenis_id: jenis_id,
+                        lat: lat,
+                        lng: lng,
+                        deskripsi: deskripsi
+                    },
+                    success: function(hasil) {
+                        if (hasil.success == true) {
+                            // alert(hasil.message);
+                            toastr.options = {
+                                "closeButton": true,
+                                "progressBar": true
+                            }
+                            toastr.success("Data berhasil disimpan.");
+                        } else {
+                            // alert("Error !");
+                            toastr.options = {
+                                "closeButton": true,
+                                "progressBar": true
+                            }
+                            toastr.error("Error !");
                         }
-                        toastr.error("Error !");
                     }
-                }
-            });
+                });
 
-        }
-    });
-</script>
+            }
+        });
+    </script>
 @endsection
